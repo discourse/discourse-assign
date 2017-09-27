e7ca1ffbf7e9c737e7bf305f80973beb10b58fd5
Robin Ward <robin.ward@gmail.com>
2017-09-27 11:01:29 -0400

    Revert "Fix field not being preloaded in multisite setup."
    This reverts commit 83b4e9fe3f8a6e486c1ef5a2cdfb8aa61197313b.

--- plugin.rb
+++ plugin.rb
@@ -33,7 +33,12 @@ after_initialize do
     end
   end
 
-  TopicList.preloaded_custom_fields << "assigned_to_id"
+  # We can remove this check once this method is stable
+  if respond_to?(:add_preloaded_topic_list_custom_field)
+    add_preloaded_topic_list_custom_field('assigned_to_id')
+  else
+    TopicList.preloaded_custom_fields << "assigned_to_id"
+  end
 
   TopicList.on_preload do |topics, topic_list|
     if SiteSetting.assign_enabled?
