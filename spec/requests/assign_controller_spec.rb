f109be3ec71dd154a32351e4c075f3986bbe7a2d
Saurabh Patel <saurabh.finch@gmail.com>
2018-12-06 13:47:23 +0700

    FIX: add translations and check for message and if assignment is successful in spec

--- spec/requests/assign_controller_spec.rb
+++ spec/requests/assign_controller_spec.rb
@@ -6,16 +6,17 @@ RSpec.describe DiscourseAssign::AssignController do
   let(:post) { Fabricate(:post) }
   let(:user2) { Fabricate(:active_user) }
 
-  context 'assign' do
+  context '#assign' do
 
     it 'assigns topic to a user' do
       sign_in(user)
 
-      put '/assign/assign', params: {
+      put '/assign/assign.json', params: {
         topic_id: post.topic_id, username: user2.username
       }
 
       expect(response.status).to eq(200)
+      expect(post.topic.reload.custom_fields['assigned_to_id']).to eq(user2.id.to_s)
     end
 
     it 'fails to assign topic to the user if its already assigned to the same user' do
@@ -26,12 +27,14 @@ RSpec.describe DiscourseAssign::AssignController do
       }
 
       expect(response.status).to eq(200)
+      expect(post.topic.reload.custom_fields['assigned_to_id']).to eq(user2.id.to_s)
 
       put '/assign/assign.json', params: {
         topic_id: post.topic_id, username: user2.username
       }
 
       expect(response.status).to eq(400)
+      expect(JSON.parse(response.body)['failed']).to eq(I18n.t('discourse_assign.already_assigned', username: user2.username))
     end
 
   end
