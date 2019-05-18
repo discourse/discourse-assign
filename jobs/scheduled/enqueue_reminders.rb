16d91c4602a72f631a41a600b94d254cf8a2c338
romanrizzi <rizziromanalejandro@gmail.com>
2019-05-18 20:49:13 -0300

    Sanitize query and user query single

--- jobs/scheduled/enqueue_reminders.rb
+++ jobs/scheduled/enqueue_reminders.rb
@@ -17,36 +17,36 @@ module Jobs
 
     def user_ids
       global_frequency = SiteSetting.remind_assigns_frequency
-      frequency = "COALESCE(user_frequency.value, '#{global_frequency}')::INT"
+      frequency = ActiveRecord::Base.sanitize_sql("COALESCE(user_frequency.value, '#{global_frequency}')::INT")
 
-      TopicCustomField
-        .joins(<<~SQL
-          LEFT OUTER JOIN user_custom_fields AS last_reminder
-          ON topic_custom_fields.value::INT = last_reminder.user_id
-          AND last_reminder.name = '#{PendingAssignsReminder::REMINDED_AT}'
-        SQL
+      DB.query_single(<<~SQL
+        SELECT topic_custom_fields.value
+        FROM topic_custom_fields
+
+        LEFT OUTER JOIN user_custom_fields AS last_reminder
+        ON topic_custom_fields.value::INT = last_reminder.user_id
+        AND last_reminder.name = '#{PendingAssignsReminder::REMINDED_AT}'
+
+        LEFT OUTER JOIN user_custom_fields AS user_frequency
+        ON topic_custom_fields.value::INT = user_frequency.user_id
+        AND user_frequency.name = '#{PendingAssignsReminder::REMINDERS_FREQUENCY}'
+
+        INNER JOIN users ON topic_custom_fields.value::INT = users.id
+        INNER JOIN topics ON topics.id = topic_custom_fields.topic_id AND (topics.deleted_at IS NULL)
+
+        WHERE (users.moderator OR users.admin)
+        AND #{frequency} > 0
+        AND (
+          last_reminder.value IS NULL OR
+          last_reminder.value::TIMESTAMP <= CURRENT_TIMESTAMP - ('1 MINUTE'::INTERVAL * #{frequency})
         )
-        .joins("INNER JOIN users ON topic_custom_fields.value::INT = users.id")
-        .where("users.moderator OR users.admin")
-        .joins(<<~SQL
-          LEFT OUTER JOIN user_custom_fields AS user_frequency
-          ON topic_custom_fields.value::INT = user_frequency.user_id
-          AND user_frequency.name = '#{PendingAssignsReminder::REMINDERS_FREQUENCY}'
-        SQL
-        )
-        .where(<<~SQL
-          #{frequency} > 0 AND
-          (
-            last_reminder.value IS NULL OR
-            last_reminder.value::TIMESTAMP <= CURRENT_TIMESTAMP - ('1 MINUTE'::INTERVAL * #{frequency})
-          )
-        SQL
-        ).where("topic_custom_fields.updated_at::TIMESTAMP <= CURRENT_TIMESTAMP - ('1 MINUTE'::INTERVAL * #{frequency})")
-        .where(name: TopicAssigner::ASSIGNED_TO_ID)
-        .joins(:topic)
-        .group('topic_custom_fields.value')
-        .having('COUNT(topic_custom_fields.value) > 1')
-        .pluck('topic_custom_fields.value')
+        AND topic_custom_fields.updated_at::TIMESTAMP <= CURRENT_TIMESTAMP - ('1 MINUTE'::INTERVAL * #{frequency})
+        AND topic_custom_fields.name = '#{TopicAssigner::ASSIGNED_TO_ID}'
+
+        GROUP BY topic_custom_fields.value
+        HAVING COUNT(topic_custom_fields.value) > 1
+      SQL
+      )
     end
   end
 end
