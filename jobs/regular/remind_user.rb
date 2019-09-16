d59e7fe1fbe95789324010b3729d0589a8a9789f
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2019-09-17 03:39:32 +1000

    FIX: Use top-level namespace for base classes (#52)

--- jobs/regular/remind_user.rb
+++ jobs/regular/remind_user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Jobs
-  class RemindUser < Jobs::Base
+  class RemindUser < ::Jobs::Base
     sidekiq_options queue: 'low'
 
     def execute(args)
--- jobs/scheduled/enqueue_reminders.rb
+++ jobs/scheduled/enqueue_reminders.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Jobs
-  class EnqueueReminders < Jobs::Scheduled
+  class EnqueueReminders < ::Jobs::Scheduled
     every 1.day
 
     def execute(_args)
