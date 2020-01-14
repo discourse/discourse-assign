6db4586ca11131411dc0e1170000a08576b54948
Gerhard Schlager <mail@gerhard-schlager.at>
2020-01-14 18:19:42 +0100

    DEV: Specs were broken

--- spec/serializers/topic_list_serializer_spec.rb
+++ spec/serializers/topic_list_serializer_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require_relative '../support/assign_allowed_group'
 
 RSpec.describe TopicListSerializer do
   fab!(:user) { Fabricate(:user) }
