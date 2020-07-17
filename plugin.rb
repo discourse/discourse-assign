7d43852bcf09957175e4d41a355516ae7d9634cb
Ahmed <ahmedgagan6@gmail.com>
2020-07-17 18:46:11 +0530

    member's assignments count added

--- plugin.rb
+++ plugin.rb
@@ -40,14 +40,6 @@ after_initialize do
     RemindAssignsFrequencySiteSettings.values
   end
 
-  # add_to_serializer(:group_user, :assignments_count, false) do
-  #   object.assignments_count
-  # end
-
-  # add_to_serializer(:group_user, :include_assignments_count) do
-  #   object.can_assign?
-  # end
-
   add_to_serializer(:group_show, :assignment_count) do
     Topic.joins("JOIN topic_custom_fields tcf ON topics.id = tcf.topic_id AND tcf.name = 'assigned_to_id' AND tcf.value IS NOT NULL")
       .where("tcf.value IN (SELECT group_users.user_id::varchar(255) FROM group_users WHERE (group_id IN (SELECT id FROM groups WHERE name = ?)))", object.name).count
