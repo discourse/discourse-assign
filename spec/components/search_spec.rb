6b9be6e49ecce04febeebd0461b457119448162e
Ahmed <ahmedgagan6@gmail.com>
2020-08-19 01:10:23 +0530

    rspec issue solved

--- spec/components/search_spec.rb
+++ spec/components/search_spec.rb
@@ -5,34 +5,37 @@ require_relative '../support/assign_allowed_group'
 
 describe Search do
 
+  fab!(:user) { Fabricate(:active_user) }
+  fab!(:user2) { Fabricate(:user) }
+
   before do
     SearchIndexer.enable
     SiteSetting.assign_enabled = true
   end
 
-  fab!(:user) { Fabricate(:active_user) }
-  fab!(:user2) { Fabricate(:user) }
-
   context 'Advanced search' do
     include_context 'A group that is allowed to assign'
 
-    fab!(:post1) { Fabricate(:post) }
-    fab!(:post2) { Fabricate(:post) }
-    fab!(:post3) { Fabricate(:post) }
+    let(:post1) { Fabricate(:post) }
+    let(:post2) { Fabricate(:post) }
+    let(:post3) { Fabricate(:post) }
 
     before do
-
       add_to_assign_allowed_group(user)
       add_to_assign_allowed_group(user2)
 
       TopicAssigner.new(post1.topic, user).assign(user)
       TopicAssigner.new(post2.topic, user).assign(user2)
+      TopicAssigner.new(post3.topic, user).assign(user)
     end
 
     it 'can find by status' do
-      expect(Search.execute('in:assigned', guardian: Guardian.new(user)).posts.length).to eq(2)
+      expect(Search.execute('in:assigned', guardian: Guardian.new(user)).posts.length).to eq(3)
+
+      TopicAssigner.new(post3.topic, user).unassign
+
       expect(Search.execute('in:not_assigned', guardian: Guardian.new(user)).posts.length).to eq(1)
-      expect(Search.execute("assigned:#{user1.username}", guardian: Guardian.new(user)).posts.length).to eq(1)
+      expect(Search.execute("assigned:#{user.username}", guardian: Guardian.new(user)).posts.length).to eq(1)
     end
 
   end
