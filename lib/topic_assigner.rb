d249ba01277c5c4871bf65eff3df47814f145583
Jarek Radosz <jradosz@gmail.com>
2021-04-29 19:18:54 +0200

    DEV: Delete custom fields instead of setting nil

--- lib/topic_assigner.rb
+++ lib/topic_assigner.rb
@@ -279,8 +279,8 @@ class ::TopicAssigner
       end
 
       # clean up in memory object
-      @topic.custom_fields[ASSIGNED_TO_ID] = nil
-      @topic.custom_fields[ASSIGNED_BY_ID] = nil
+      @topic.custom_fields.delete(ASSIGNED_TO_ID)
+      @topic.custom_fields.delete(ASSIGNED_BY_ID)
 
       # nothing to do here
       return if !assigned_to_id
