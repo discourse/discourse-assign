5372fd3a106618f6c8e598b87ae222a1d5fac58e
Jarek Radosz <jradosz@gmail.com>
2021-05-04 19:51:26 +0200

    DEV: Change migration version to 6.0

--- db/post_migrate/20210429154319_remove_nil_custom_fields.rb
+++ db/post_migrate/20210429154319_remove_nil_custom_fields.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class RemoveNilCustomFields < ActiveRecord::Migration[6.1]
+class RemoveNilCustomFields < ActiveRecord::Migration[6.0]
   def up
     execute <<~SQL
       DELETE FROM topic_custom_fields
