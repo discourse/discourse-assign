b0363b214109c97e82632a2f15254b3ff89603b9
jjaffeux <j.jaffeux@gmail.com>
2021-06-24 16:19:22 +0200

    simpler check

--- plugin.rb
+++ plugin.rb
@@ -586,11 +586,7 @@ after_initialize do
       triggerables %i[point_in_time recurring]
 
       script do |context, fields|
-        begin
-          next unless SiteSetting.assign_enabled
-        rescue NoMethodError
-          next
-        end
+        next unless SiteSetting.try(:assign_enabled)
 
         next unless group_id = fields.dig('assignees_group', 'group_id')
         next unless group = Group.find_by(id: group_id)
