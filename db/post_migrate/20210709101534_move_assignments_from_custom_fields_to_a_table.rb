b0a480fd357590656da05707115b4408bab7e791
Jarek Radosz <jradosz@gmail.com>
2021-07-14 20:45:31 +0200

    FIX: CF to table migration was incorrect (#171)
    The order of values in the `INSERT` query was invalid (`assigned_to_id` and `topic_id` were swapped)
    
    Co-authored-by: David Taylor <david@taylorhq.com>

--- db/post_migrate/20210709101534_move_assignments_from_custom_fields_to_a_table.rb
+++ db/post_migrate/20210709101534_move_assignments_from_custom_fields_to_a_table.rb
@@ -2,18 +2,7 @@
 
 class MoveAssignmentsFromCustomFieldsToATable < ActiveRecord::Migration[6.1]
   def up
-    execute <<~SQL
-      INSERT INTO assignments (topic_id, assigned_by_user_id, assigned_to_id, created_at, updated_at)
-      SELECT (
-        SELECT value::integer assigned_to_id
-        FROM topic_custom_fields tcf1
-        WHERE tcf1.name = 'assigned_to_id' AND tcf1.topic_id = tcf2.topic_id
-      ), value::integer assgined_by_id, topic_id, created_at, updated_at
-      FROM topic_custom_fields tcf2
-      WHERE name = 'assigned_by_id'
-      ORDER BY created_at DESC
-      ON CONFLICT DO NOTHING
-    SQL
+    # No-op, this migration was invalid
   end
 
   def down
