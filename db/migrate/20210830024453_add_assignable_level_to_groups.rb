e14b2eb25231df4fbc4bc1f10274c1b8cbd05b15
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2021-09-06 11:45:40 +1000

    FIX: null false

--- db/migrate/20210830024453_add_assignable_level_to_groups.rb
+++ db/migrate/20210830024453_add_assignable_level_to_groups.rb
@@ -2,6 +2,6 @@
 
 class AddAssignableLevelToGroups < ActiveRecord::Migration[6.1]
   def change
-    add_column :groups, :assignable_level, :integer, default: 0
+    add_column :groups, :assignable_level, :integer, default: 0, null: false
   end
 end
