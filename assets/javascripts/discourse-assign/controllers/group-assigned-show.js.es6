discourse/discourse-assign

commit ab0c63b1fbc20a3ade644e3c41db308b45fddbef

DEV: Fix a computed property override (#239)

assets/javascripts/discourse-assign/controllers/group-assigned-show.js.es6
@@ -1,6 +1,5 @@
 import UserTopicsList from "discourse/controllers/user-topics-list";
-import { alias } from "@ember/object/computed";
-import discourseComputed from "discourse-common/utils/decorators";
+import { alias, readOnly } from "@ember/object/computed";
 import discourseDebounce from "discourse-common/lib/debounce";
 import { INPUT_DELAY } from "discourse-common/config/environment";
 import { inject as controller } from "@ember/controller";
@@ -19,10 +18,7 @@ export default UserTopicsList.extend({
 
   queryParams: ["order", "ascending", "search"],
 
-  @discourseComputed("search")
-  searchTerm(search) {
-    return search;
-  },
+  searchTerm: readOnly("search"),
 
   _setSearchTerm(searchTerm) {
     this.set("search", searchTerm);

assets/javascripts/discourse-assign/controllers/user-activity-assigned.js.es6
@@ -8,6 +8,7 @@ import { action } from "@ember/object";
 import getURL from "discourse-common/lib/get-url";
 import { iconHTML } from "discourse-common/lib/icon-library";
 import I18n from "I18n";
+import { readOnly } from "@ember/object/computed";
 
 export default UserTopicsList.extend({
   user: controller(),
@@ -17,9 +18,11 @@ export default UserTopicsList.extend({
   ascending: false,
   search: "",
 
-  @discourseComputed("search")
-  searchTerm(search) {
-    return search;
+  searchTerm: readOnly("search"),
+
+  _setSearchTerm(searchTerm) {
+    this.set("search", searchTerm);
+    this.refreshModel();
   },
 
   @discourseComputed("model.topics.length", "search")
@@ -33,11 +36,6 @@ export default UserTopicsList.extend({
       preferencesUrl: getURL("/my/preferences/notifications"),
       icon: iconHTML("user-plus"),
     }).htmlSafe();
-  },
-
-  _setSearchTerm(searchTerm) {
-    this.set("search", searchTerm);
-    this.refreshModel();
   },
 
   refreshModel() {

assets/javascripts/discourse-assign/routes/group-assigned-show.js.es6
@@ -39,7 +39,7 @@ export default DiscourseRoute.extend({
   setupController(controller, model) {
     controller.setProperties({
       model,
-      searchTerm: this.currentModel.params.search,
+      search: this.currentModel.params.search,
     });
   },
 

commit d7c968fe0d6bd95cf4d715a6bbc4faf0ed0f0d6f

FIX: Broken assigned topics search
regressed in #239

assets/javascripts/discourse-assign/controllers/group-assigned-show.js.es6
@@ -1,5 +1,5 @@
 import UserTopicsList from "discourse/controllers/user-topics-list";
-import { alias, readOnly } from "@ember/object/computed";
+import { alias } from "@ember/object/computed";
 import discourseDebounce from "discourse-common/lib/debounce";
 import { INPUT_DELAY } from "discourse-common/config/environment";
 import { inject as controller } from "@ember/controller";
@@ -17,8 +17,6 @@ export default UserTopicsList.extend({
   canBulkSelect: alias("currentUser.staff"),
 
   queryParams: ["order", "ascending", "search"],
-
-  searchTerm: readOnly("search"),
 
   _setSearchTerm(searchTerm) {
     this.set("search", searchTerm);

assets/javascripts/discourse-assign/controllers/user-activity-assigned.js.es6
@@ -8,7 +8,6 @@ import { action } from "@ember/object";
 import getURL from "discourse-common/lib/get-url";
 import { iconHTML } from "discourse-common/lib/icon-library";
 import I18n from "I18n";
-import { readOnly } from "@ember/object/computed";
 
 export default UserTopicsList.extend({
   user: controller(),
@@ -17,8 +16,6 @@ export default UserTopicsList.extend({
   order: "",
   ascending: false,
   search: "",
-
-  searchTerm: readOnly("search"),
 
   _setSearchTerm(searchTerm) {
     this.set("search", searchTerm);

assets/javascripts/discourse/templates/group-topics-list.hbs
@@ -2,7 +2,7 @@
   <div class="inline-form full-width">
     {{input
       class="no-blur"
-      value=searchTerm
+      value=(readonly search)
       placeholder=(i18n "discourse_assign.topic_search_placeholder")
       input=(action "onChangeFilter" value="target.value")
       autocomplete="discourse"

assets/javascripts/discourse/templates/group/assigned.hbs
@@ -5,7 +5,7 @@
         {{input
           type="text"
           placeholder=(i18n "discourse_assign.sidebar_name_filter_placeholder")
-          value=filterName
+          value=(readonly filterName)
           class="search"
           input=(action "onChangeFilterName" value="target.value")
         }}

assets/javascripts/discourse/templates/user-assigned-topics.hbs
@@ -10,7 +10,7 @@
     <div class="inline-form full-width">
       {{input
         class="no-blur"
-        value=searchTerm
+        value=(readonly search)
         placeholder=(i18n "discourse_assign.topic_search_placeholder")
         input=(action "onChangeFilter" value="target.value")
         autocomplete="discourse"
@@ -39,4 +39,3 @@
     {{conditional-loading-spinner condition=model.loadingMore}}
   {{/load-more}}
 {{/if}}
-