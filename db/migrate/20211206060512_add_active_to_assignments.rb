728c03522e20865a7bfaef6e8e2b567afe55c46b
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2021-12-08 08:36:57 +0100

    FIX: index and annotate

--- db/migrate/20211206060512_add_active_to_assignments.rb
+++ db/migrate/20211206060512_add_active_to_assignments.rb
@@ -3,5 +3,6 @@
 class AddActiveToAssignments < ActiveRecord::Migration[6.1]
   def change
     add_column :assignments, :active, :boolean, default: true
+    add_index :assignments, :active
   end
 end
