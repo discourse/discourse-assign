896d0e34458c5a0ee09add7d888f647875783cd2
Jan Cernik <66427541+jancernik@users.noreply.github.com>
2023-01-17 08:09:35 -0300

    FIX: Broken posts when assigning a closed topic (#425)
    The previous behavior was:
    1. Assign a topic to someone
    2. Close the topic with the setting unassign_on_close enabled
    3. Assign the topic to the same user as before
    
    This caused small broken or empty posts to be displayed.
    
    This commit also introduces system specs

---
+++ spec/system/assign_topic_spec.rb
@@ -0,0 +1,114 @@
+# frozen_string_literal: true
+
+describe "Assign | Assigning topics", type: :system, js: true do
+  let(:topic_page) { PageObjects::Pages::Topic.new }
+  let(:assign_modal) { PageObjects::Modals::Assign.new }
+  fab!(:staff_user) { Fabricate(:user, groups: [Group[:staff]]) }
+  fab!(:admin) { Fabricate(:admin) }
+  fab!(:topic) { Fabricate(:topic) }
+  fab!(:post) { Fabricate(:post, topic: topic) }
+
+  before do
+    SiteSetting.assign_enabled = true
+    sign_in(admin)
+  end
+
+  describe "with open topic" do
+    it "can assign and unassign" do
+      visit "/t/#{topic.id}"
+
+      topic_page.click_assign_topic
+      assign_modal.assignee = staff_user
+      assign_modal.confirm
+
+      expect(topic_page).to have_assigned(user: staff_user, at_post: 2)
+      expect(find("#topic .assigned-to")).to have_content(staff_user.username)
+
+      topic_page.click_unassign_topic
+
+      expect(topic_page).to have_unassigned(user: staff_user, at_post: 3)
+      expect(page).to have_no_css("#topic .assigned-to")
+    end
+
+    context "when unassign_on_close is set to true" do
+      before { SiteSetting.unassign_on_close = true }
+
+      it "unassigns the topic on close" do
+        visit "/t/#{topic.id}"
+
+        topic_page.click_assign_topic
+        assign_modal.assignee = staff_user
+        assign_modal.confirm
+
+        expect(topic_page).to have_assigned(user: staff_user, at_post: 2)
+
+        find(".topic-footer-main-buttons .toggle-admin-menu").click
+        find(".topic-admin-close").click
+
+        expect(find("#post_3")).to have_content(
+          I18n.t("js.action_codes.closed.enabled", when: "just now"),
+        )
+        expect(page).to have_no_css("#post_4")
+        expect(page).to have_no_css("#topic .assigned-to")
+      end
+
+      it "can assign the previous assignee" do
+        visit "/t/#{topic.id}"
+
+        topic_page.click_assign_topic
+        assign_modal.assignee = staff_user
+        assign_modal.confirm
+
+        expect(topic_page).to have_assigned(user: staff_user, at_post: 2)
+
+        find(".topic-footer-main-buttons .toggle-admin-menu").click
+        find(".topic-admin-close").click
+
+        expect(find("#post_3")).to have_content(
+          I18n.t("js.action_codes.closed.enabled", when: "just now"),
+        )
+        expect(page).to have_no_css("#post_4")
+        expect(page).to have_no_css("#topic .assigned-to")
+
+        topic_page.click_assign_topic
+        assign_modal.assignee = staff_user
+        assign_modal.confirm
+
+        expect(page).to have_no_css("#post_4")
+        expect(find("#topic .assigned-to")).to have_content(staff_user.username)
+      end
+
+      context "when reassign_on_open is set to true" do
+        before { SiteSetting.reassign_on_open = true }
+
+        it "reassigns the topic on open" do
+          visit "/t/#{topic.id}"
+
+          topic_page.click_assign_topic
+          assign_modal.assignee = staff_user
+          assign_modal.confirm
+
+          expect(topic_page).to have_assigned(user: staff_user, at_post: 2)
+
+          find(".topic-footer-main-buttons .toggle-admin-menu").click
+          find(".topic-admin-close").click
+
+          expect(find("#post_3")).to have_content(
+            I18n.t("js.action_codes.closed.enabled", when: "just now"),
+          )
+          expect(page).to have_no_css("#post_4")
+          expect(page).to have_no_css("#topic .assigned-to")
+
+          find(".topic-footer-main-buttons .toggle-admin-menu").click
+          find(".topic-admin-open").click
+
+          expect(find("#post_4")).to have_content(
+            I18n.t("js.action_codes.closed.disabled", when: "just now"),
+          )
+          expect(page).to have_no_css("#post_5")
+          expect(find("#topic .assigned-to")).to have_content(staff_user.username)
+        end
+      end
+    end
+  end
+end
---
+++ spec/system/page_objects/pages/topic.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module PageObjects
+  module Pages
+    class Topic < PageObjects::Pages::Base
+      def click_assign_topic
+        find("#topic-footer-button-assign").click
+      end
+
+      def click_unassign_topic
+        find("#topic-footer-dropdown-reassign").click
+        find("[data-value='unassign']").click
+      end
+
+      def click_edit_topic_assignment
+        find("#topic-footer-dropdown-reassign").click
+        find("[data-value='reassign']").click
+      end
+
+      def has_assigned?(args)
+        has_assignment_action?(action: "assigned", **args)
+      end
+
+      def has_unassigned?(args)
+        has_assignment_action?(action: "unassigned", **args)
+      end
+
+      def has_assignment_action?(args)
+        assignee = args[:group]&.name || args[:user]&.username
+        container = args[:at_post] ? find("#post_#{args[:at_post]}") : page
+        container.has_content?(
+          I18n.t("js.action_codes.#{args[:action]}", who: "@#{assignee}", when: "just now"),
+        )
+      end
+    end
+  end
+end
