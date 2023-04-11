29874f1c9aa89f44f18da9cd8f749c381244b61e
Jan Cernik <jancernik12@gmail.com>
2023-04-10 22:09:58 -0300

    Revert "FIX: Show user status tooltip on the assign modal"
    This reverts commit ff9ef36a28954d62bb0851bbfa2eff8f2ee72139.

--- assets/javascripts/discourse/components/assignee-chooser-row.js
+++ assets/javascripts/discourse/components/assignee-chooser-row.js
@@ -1,7 +1,3 @@
 import EmailGroupUserChooserRow from "select-kit/components/email-group-user-chooser-row";
 
-export default EmailGroupUserChooserRow.extend({
-  stopPropagation(instance, event) {
-    event.stopPropagation();
-  },
-});
+export default EmailGroupUserChooserRow.extend();
