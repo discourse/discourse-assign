e2273de0da415debdfb7c8e2a8a49a685d8d2321
Isaac Janzen <isaac.janzen@discourse.org>
2023-04-26 14:53:32 -0500

    clean up key down

--- assets/javascripts/discourse-assign/controllers/assign-user.js
+++ assets/javascripts/discourse-assign/controllers/assign-user.js
@@ -72,7 +72,7 @@ export default Controller.extend(ModalFunctionality, {
   },
 
   @action
-  handleTextAreaKeydown(value, event) {
+  handleTextAreaKeyUp(value, event) {
     if ((event.ctrlKey || event.metaKey) && event.key === "Enter") {
       this.assign();
     }
