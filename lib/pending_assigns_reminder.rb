968751c8a94974f2edf29b612ccb6dfafa6020f9
Jarek Radosz <jradosz@gmail.com>
2023-05-26 15:40:40 +0200

    DEV: Fix a core deprecation (#475)

--- lib/pending_assigns_reminder.rb
+++ lib/pending_assigns_reminder.rb
@@ -110,7 +110,7 @@ class PendingAssignsReminder
   end
 
   def time_in_words_for(topic)
-    FreedomPatches::Rails4.distance_of_time_in_words(
+    AgeWords.distance_of_time_in_words(
       Time.zone.now,
       topic.assigned_at.to_time,
       false,
