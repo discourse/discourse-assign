d24859bf1509d957407eb560c57656ef855e19f6
Isaac Janzen <isaac.janzen@discourse.org>
2023-06-27 11:53:58 -0500

    DEV: Convert dismiss modal to component-based API

--- assets/javascripts/discourse/components/user-menu/assigns-list.js
+++ assets/javascripts/discourse/components/user-menu/assigns-list.js
@@ -4,7 +4,6 @@ import UserMenuNotificationItem from "discourse/lib/user-menu/notification-item"
 import UserMenuAssignItem from "discourse/plugins/discourse-assign/discourse-assign/lib/user-menu/assign-item";
 import Notification from "discourse/models/notification";
 import I18n from "I18n";
-import showModal from "discourse/lib/show-modal";
 import Topic from "discourse/models/topic";
 import UserMenuAssignsListEmptyState from "./assigns-list-empty-state";
 
@@ -37,6 +36,25 @@ export default class UserMenuAssignNotificationsList extends UserMenuNotificatio
     return UserMenuAssignsListEmptyState;
   }
 
+  get alwaysRenderDismissConfirmation() {
+    return true;
+  }
+
+  get _unreadAssignedNotificationsCount() {
+    const key = `grouped_unread_notifications.${this.site.notification_types.assigned}`;
+    // we're retrieving the value with get() so that Ember tracks the property
+    // and re-renders the UI when it changes.
+    // we can stop using `get()` when the User model is refactored into native
+    // class with @tracked properties.
+    return this.currentUser.get(key) || 0;
+  }
+
+  get dismissConfirmationText() {
+    return I18n.t("notifications.dismiss_confirmation.body.assigns", {
+      count: this._unreadAssignedNotificationsCount,
+    });
+  }
+
   async fetchItems() {
     const data = await ajax("/assign/user-menu-assigns.json");
     const content = [];
@@ -58,25 +76,5 @@ export default class UserMenuAssignNotificationsList extends UserMenuNotificatio
     await Topic.applyTransformations(topics);
     content.push(...topics.map((assign) => new UserMenuAssignItem({ assign })));
     return content;
-  }
-
-  dismissWarningModal() {
-    const modalController = showModal("dismiss-notification-confirmation");
-    modalController.set(
-      "confirmationMessage",
-      I18n.t("notifications.dismiss_confirmation.body.assigns", {
-        count: this._unreadAssignedNotificationsCount,
-      })
-    );
-    return modalController;
-  }
-
-  get _unreadAssignedNotificationsCount() {
-    const key = `grouped_unread_notifications.${this.site.notification_types.assigned}`;
-    // we're retrieving the value with get() so that Ember tracks the property
-    // and re-renders the UI when it changes.
-    // we can stop using `get()` when the User model is refactored into native
-    // class with @tracked properties.
-    return this.currentUser.get(key) || 0;
   }
 }
