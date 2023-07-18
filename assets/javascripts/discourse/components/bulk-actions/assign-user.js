458306fa6b7bb134a338a7a012b4230f75d133a8
Jarek Radosz <jradosz@gmail.com>
2023-07-18 14:15:16 +0200

    DEV: Use the new bulk topic actions api (#491)

---
+++ assets/javascripts/discourse/components/bulk-actions/assign-user.js
@@ -0,0 +1,20 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+
+export default class AssignUser extends Component {
+  model = {};
+
+  // `submit` property will be mutated by the `AssignUserForm` component
+  formApi = {
+    submit() {},
+  };
+
+  @action
+  async assign() {
+    return this.args.performAndRefresh({
+      type: "assign",
+      username: this.model.username,
+      note: this.model.note,
+    });
+  }
+}
