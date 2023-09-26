a57560410de59abd97c2b1e4609c9f6d747495cb
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2023-09-26 11:34:18 +1000

    FIX: update after changes

--- spec/fabricators/assign_hook_fabricator.rb
+++ spec/fabricators/assign_hook_fabricator.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 Fabricator(:assign_web_hook, from: :web_hook) do
-  transient assigned_hook: WebHookEventType.find_by(name: "assigned"),
-            unassigned_hook: WebHookEventType.find_by(name: "unassigned")
-
-  after_build do |web_hook, transients|
-    web_hook.web_hook_event_types = [transients[:assigned_hook], transients[:unassigned_hook]]
+  after_build do |web_hook|
+    web_hook.web_hook_event_types = WebHookEventType.where(name: %w[assigned unassigned])
   end
 end
