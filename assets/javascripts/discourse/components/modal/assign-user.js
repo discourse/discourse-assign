88ee6aeaf830b71005160e46bb1c2a48ba3af3ce
Jarek Radosz <jradosz@gmail.com>
2023-12-07 14:05:50 +0100

    track modal model updates

--- assets/javascripts/discourse/components/modal/assign-user.js
+++ assets/javascripts/discourse/components/modal/assign-user.js
@@ -1,10 +1,13 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
+import { TrackedObject } from "@ember-compat/tracked-built-ins";
 import I18n from "I18n";
 
 export default class AssignUser extends Component {
   @service taskActions;
+
+  model = new TrackedObject(this.args.model);
 
   // `submit` property will be mutated by the `AssignUserForm` component
   formApi = {
@@ -14,7 +17,7 @@ export default class AssignUser extends Component {
   get title() {
     let i18nSuffix;
 
-    switch (this.args.model.targetType) {
+    switch (this.model.targetType) {
       case "Post":
         i18nSuffix = "_post_modal";
         break;
@@ -25,7 +28,7 @@ export default class AssignUser extends Component {
 
     return I18n.t(
       `discourse_assign.assign${i18nSuffix}.${
-        this.args.model.reassign ? "reassign_title" : "title"
+        this.model.reassign ? "reassign_title" : "title"
       }`
     );
   }
@@ -33,6 +36,6 @@ export default class AssignUser extends Component {
   @action
   async onSubmit() {
     this.args.closeModal();
-    await this.taskActions.assign(this.args.model);
+    await this.taskActions.assign(this.model);
   }
 }
