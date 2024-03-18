a4a3432b86d7b1c4d25b2cf879369674c89b2e69
Blake Erickson <o.blakeerickson@gmail.com>
2024-03-18 15:41:23 -0600

    white space

--- spec/system/page_objects/components/topic_list_header.rb
+++ spec/system/page_objects/components/topic_list_header.rb
@@ -3,7 +3,6 @@
 module PageObjects
   module Components
     class TopicListHeader < PageObjects::Components::Base
-
       def has_assign_topics_button?
         page.has_css?(bulk_select_dropdown_item("topics.bulk.assign"))
       end
@@ -11,7 +10,6 @@ module PageObjects
       def click_assign_topics_button
         find(bulk_select_dropdown_item("topics.bulk.assign")).click
       end
-
     end
   end
 end
