2abac98e18e51ab1119f42869e31eed623d54f63
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2024-04-01 23:24:41 +0400

    WIP: Move the rest into the component

--- assets/javascripts/discourse/components/assignment.js
+++ assets/javascripts/discourse/components/assignment.js
@@ -5,6 +5,7 @@ import { inject as service } from "@ember/service";
 
 export default class Assignment extends Component {
   @service siteSettings;
+  @service taskActions;
 
   constructor() {
     super(...arguments);
