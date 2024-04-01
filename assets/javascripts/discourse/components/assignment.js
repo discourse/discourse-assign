7b2b8c11515ce24fd2fd31018edffdda186cb9d7
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2024-04-01 20:43:17 +0400

    Extract <Assignment /> component

---
+++ assets/javascripts/discourse/components/assignment.js
@@ -0,0 +1,13 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+
+export default class AssignUserForm extends Component {
+  @action
+  handleTextAreaKeydown(event) {
+    if ((event.ctrlKey || event.metaKey) && event.key === "Enter") {
+      this.submit(); // fixme andrei
+    }
+  }
+}
