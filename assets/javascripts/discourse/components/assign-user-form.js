2d27c1adaa129825d813ac40788a07664e5d057b
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2024-04-02 00:59:25 +0400

    Drop unused stuff

--- assets/javascripts/discourse/components/assign-user-form.js
+++ assets/javascripts/discourse/components/assign-user-form.js
@@ -1,14 +1,8 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
 
 export default class AssignUserForm extends Component {
-  @service taskActions;
-  @service siteSettings;
-  @service capabilities;
-
-  @tracked assigneeError = false;
   @tracked showValidationErrors = false;
 
   constructor() {
