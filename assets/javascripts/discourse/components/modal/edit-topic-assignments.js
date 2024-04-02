b84b6aa9df5015733db9a92833646a61784fc424
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2024-04-02 20:13:07 +0400

    WIP: edit topic assignments modal

--- assets/javascripts/discourse/components/modal/edit-topic-assignments.js
+++ assets/javascripts/discourse/components/modal/edit-topic-assignments.js
@@ -1,28 +1,43 @@
 import Component from "@glimmer/component";
-import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import I18n from "I18n";
 
+// fixme andrei validation?
 export default class EditTopicAssignments extends Component {
   @service taskActions;
   @tracked assignments = [];
 
-  // fixme andrei
-  // `submit` property will be mutated by the `AssignUserForm` component
-  formApi = {
-    submit() {},
-  };
+  constructor() {
+    super(...arguments);
+    const topicAssignment = {
+      username: "",
+      group_name: "",
+      status: "",
+      note: ""
+    };
+    this.assignments.push(topicAssignment);
+  }
 
   get title() {
-    const title = this.args.model.topic.isAssigned()
+    const title = this.topic.isAssigned()
       ? "reassign_title"
       : "title";
     return I18n.t(`discourse_assign.assign_modal.${title}`);
   }
 
+  get topic() {
+    return this.args.model.topic;
+  }
+
+  get topicAssignment() {
+    return this.assignments[0];
+  }
+
   @action
-  async onSubmit() {
+  async submit() {
+    console.log("", this.assignments);
     throw "Not implemented"; // fixme andrei
     // this.args.closeModal();
     // await this.taskActions.assign(this.model);
