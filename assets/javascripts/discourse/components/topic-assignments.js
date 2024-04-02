661f4c362054bdab007262249576e34a7b07cfa7
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2024-04-02 21:03:35 +0400

    WIP: combobox options

--- assets/javascripts/discourse/components/topic-assignments.js
+++ assets/javascripts/discourse/components/topic-assignments.js
@@ -1,18 +1,23 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
-import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
 
 export default class TopicAssignments extends Component {
   @tracked selectedAssignmentId = 0;
+  @tracked selectedAssignment;
   TOPIC_ID = 0;
 
-  get topicAssignment() {
-    return this.args.assignments[0];
+  constructor() {
+    super(...arguments);
+    this.selectedAssignment = this.args.assignments[0]; // fixme andrei
   }
 
   get assignmentOptions() {
     return this.args.assignments.map((a) => this.#toComboBoxOption(a));
+  }
+
+  synchronizeAssignment(selectedAssignmentId) {
+    this.selectedAssignmentId = selectedAssignmentId;
+    this.selectedAssignment = this.args.assignments[0]; // fixme andrei
   }
 
   #toComboBoxOption(assignment) {
