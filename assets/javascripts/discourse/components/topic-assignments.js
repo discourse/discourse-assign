caea32de44f553875d756bd2147ae931f84fcef5
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2024-04-02 20:42:18 +0400

    WIP: combobox options

--- assets/javascripts/discourse/components/topic-assignments.js
+++ assets/javascripts/discourse/components/topic-assignments.js
@@ -4,7 +4,15 @@ import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 
 export default class TopicAssignments extends Component {
+  @tracked selectedAssignmentId = 0;
+  TOPIC_ID = 0;
+
   get topicAssignment() {
     return this.args.assignments[0];
+  }
+
+  get assignmentOptions() {
+    const topicAssignment = { id: this.TOPIC_ID, name: "Topic" };
+    return [topicAssignment];
   }
 }
