e38b8760ce0aa3219d945ea77e616c37e06c5b0f
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-11-12 00:14:32 +0900

    try_until_success

--- spec/system/page_objects/components/topic_list.rb
+++ spec/system/page_objects/components/topic_list.rb
@@ -4,11 +4,11 @@ module PageObjects
   module Components
     class TopicList < PageObjects::Components::Base
       def has_assigned_status?(topic)
-        page.has_css?("#{topic_list_item_assigned(topic)}", visible: :all)
+        try_until_success { page.has_css?("#{topic_list_item_assigned(topic)}") }
       end
 
       def has_unassigned_status?(topic)
-        page.has_no_css?("#{topic_list_item_assigned(topic)}", visible: :all)
+        try_until_success { page.has_no_css?("#{topic_list_item_assigned(topic)}") }
       end
 
       private
