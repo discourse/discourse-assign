4b015f7b64babdc3a320d22887363b978d80b0a4
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2024-11-15 06:14:21 +0800

    DEV: Add `lib` to autoload path
    Why manually require files when we can autoload? Also update
    `discourse-rubocop` to 3.8.0.

--- lib/discourse_assign/engine.rb
+++ lib/discourse_assign/engine.rb
@@ -4,5 +4,6 @@ module ::DiscourseAssign
   class Engine < ::Rails::Engine
     engine_name PLUGIN_NAME
     isolate_namespace DiscourseAssign
+    config.autoload_paths << File.join(config.root, "lib")
   end
 end
