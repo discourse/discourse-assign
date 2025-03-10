48d20f1e7637ffb599dce22ac479f67a5c861417
Juan David Martinez <juan@discourse.org>
2025-03-10 12:57:15 -0500

    possible flaky test

--- spec/system/assign_post_spec.rb
+++ spec/system/assign_post_spec.rb
@@ -11,6 +11,10 @@ describe "Assign | Assigning posts", type: :system do
 
   before do
     SiteSetting.assign_enabled = true
+    SiteSetting.prioritize_full_name_in_ux = false
+
+    # The system tests in this file are flaky and auth token related so turning this on
+    SiteSetting.verbose_auth_token_logging = true
 
     sign_in(admin)
   end
