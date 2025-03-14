3521385b8f0c8a5f13e7aa8472e75a983067b749
Jarek Radosz <jarek@cvx.dev>
2025-03-14 21:11:37 +0100

    fixup

--- assets/javascripts/discourse/connectors/category-custom-settings/assign-settings.js
+++ assets/javascripts/discourse/connectors/category-custom-settings/assign-settings.js
@@ -3,10 +3,10 @@ import { action } from "@ember/object";
 
 export default class AssignSettings extends Component {
   @action
-  onChangeSetting(value) {
+  onChangeSetting(event) {
     this.set(
       "outletArgs.category.custom_fields.enable_unassigned_filter",
-      value ? "true" : "false"
+      event.target.checked ? "true" : "false"
     );
   }
 }
