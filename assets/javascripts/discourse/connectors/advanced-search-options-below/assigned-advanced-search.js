3ff6bf8aa2859e92b0cf2f6e948f396be2b02973
Jarek Radosz <jradosz@gmail.com>
2025-03-17 11:55:37 +0100

    DEV: Update linting (#644)

--- assets/javascripts/discourse/connectors/advanced-search-options-below/assigned-advanced-search.js
+++ assets/javascripts/discourse/connectors/advanced-search-options-below/assigned-advanced-search.js
@@ -1,16 +1,22 @@
+import Component from "@ember/component";
 import { action } from "@ember/object";
+import { service } from "@ember/service";
+import { classNames } from "@ember-decorators/component";
 
-export default {
-  shouldRender(args, component) {
+@classNames("assigned-advanced-search")
+export default class AssignedAdvancedSearch extends Component {
+  static shouldRender(args, component) {
     return component.currentUser?.can_assign;
-  },
+  }
+
+  @service currentUser;
 
   @action
   onChangeAssigned(value) {
-    this.onChangeSearchedTermField(
+    this.outletArgs.onChangeSearchedTermField(
       "assigned",
       "updateSearchTermForAssignedUsername",
       value
     );
-  },
-};
+  }
+}
